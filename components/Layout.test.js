00dc4911684a5d938ddd71572b4c9b8031ff9f71
Danilo Aleixo <danilo_aleixo@hotmail.com>
2020-11-08 18:49:50 -0300

    Adding test

---
+++ components/Layout.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Menu from './Menu';
+
+it('should render Menu', () => {
+  const { getByText } = render(<Menu />);
+  expect(getByText('Home')).toBeInTheDocument();
+});

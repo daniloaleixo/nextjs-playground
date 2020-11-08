5b1212738827668adc5becf95fc6d3795357849f
Danilo Aleixo <danilo_aleixo@hotmail.com>
2020-11-08 17:12:44 -0300

    Adding something
    - Added TS
    - Added some pages and a global layout

---
+++ components/Menu.tsx
@@ -0,0 +1,17 @@
+import Link from 'next/link';
+
+const Menu = () => {
+  return (
+    <nav>
+      <Link href="/">
+        <a>Home</a>
+      </Link>
+      &nbsp;
+      <Link href="/about">
+        <a>About</a>
+      </Link>
+    </nav>
+  );
+};
+
+export default Menu;
